ae8da40a7ab8c8a8766ae3f1d8a8339e3e93c9cf
sagar <sagargundalesb@gmail.com>
2021-01-02 16:11:55 +0530

    Time-4:11

--- VibrantMindsJobPortal/src/main/java/com/techhub/controller/StudentController.java
+++ VibrantMindsJobPortal/src/main/java/com/techhub/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.techhub.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.techhub.model.StudentRegisterModel;
@@ -40,11 +41,16 @@ public class StudentController {
 	}
 	
 	
+
+		
+	
+	
 	
 	
 	
 	
 	
 /*===============================================================================================*/
+
 	
 }
